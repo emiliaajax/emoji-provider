30b9438ef92c911f824ddc9fc56c199111427b1c
Emilia Hansson <eh222yn@student.lnu.se>
2022-09-30 00:27:29 +0200

    Modify methods to work for emoji code points

--- testApp/index.js
+++ testApp/index.js
@@ -1,7 +1,25 @@
 import { emojilib } from '../lib/index.js'
 
-const text = emojilib.replaceEmoticonWithUnicode('hej :D')
-console.log(text)
-const p = document.createElement('p')
-p.textContent = text
-document.querySelector('#main').appendChild(p)
+// const text = emojilib.replaceEmoticonWithUnicode('hej :D')
+// console.log(text)
+let emojis = ''
+for (const emoji of emojilib.getAllEmojis()) {
+  emojis += ' ' + emoji
+}
+
+// let tags = ''
+// for (const tag of emojilib.getAllTags()) {
+//   tags += ' :' + tag + ': '
+// }
+
+const div = document.createElement('div')
+// const pElementForTags = document.createElement('p')
+const pElementForEmojis = document.createElement('p')
+
+pElementForEmojis.textContent = emojis
+// pElementForTags.textContent = tags
+
+div.appendChild(pElementForEmojis)
+// div.appendChild(pElementForTags)
+
+document.querySelector('#main').appendChild(div)
