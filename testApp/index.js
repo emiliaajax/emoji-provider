6dcb6290d839ee16be88f415cb990ad91de4266e
Emilia Hansson <eh222yn@student.lnu.se>
2022-10-01 02:04:35 +0200

    Refactor code for getting emoji from tag

--- testApp/index.js
+++ testApp/index.js
@@ -1,21 +1,22 @@
 import { emojilib } from '../lib/index.js'
 
 const text = emojilib.replaceEmoticonWithEmoji('hej :D <3 Hur mår du? :D')
+const emoji = emojilib.getEmojiByTag('hej')
 
-let emojis = ''
-for (const emoji of emojilib.getAllEmojis()) {
-  emojis += ' ' + emoji
-}
+// let emojis = ''
+// for (const emoji of emojilib.getAllEmojis()) {
+//   emojis += ' ' + emoji
+// }
 
 // let emojisFromInputText = ''
 // for (const emoji of emojilib.getEmojisThatMatchesText('sad')) {
 //   emojisFromInputText += ' ' + emoji
 // }
 
-// let emojisFromGivenCategories = ''
-// for (const emoji of emojilib.getEmojisOnlyFromCategory('flags')) {
-//   emojisFromGivenCategories += ' ' + emoji
-// }
+let emojisFromGivenCategories = ''
+for (const emoji of emojilib.getEmojisOnlyFromCategory('flags')) {
+  emojisFromGivenCategories += ' ' + emoji
+}
 
 // let tags = ''
 // for (const tag of emojilib.getAllTags()) {
@@ -26,11 +27,14 @@ const div = document.createElement('div')
 // const pElementForTags = document.createElement('p')
 const pElementForEmojis = document.createElement('p')
 const pElementForText = document.createElement('p')
+const pElementForTag = document.createElement('p')
 
-pElementForEmojis.textContent = emojis
+pElementForEmojis.textContent = emojisFromGivenCategories
 pElementForText.textContent = text
+pElementForTag.textContent = emoji
 // pElementForTags.textContent = tags
 
+div.appendChild(pElementForTag)
 div.appendChild(pElementForText)
 div.appendChild(pElementForEmojis)
 // div.appendChild(pElementForTags)
