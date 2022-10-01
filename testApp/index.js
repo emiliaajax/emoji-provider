63b24237731c8f64f97dfdf72468591b4e01d1f3
Emilia Hansson <eh222yn@student.lnu.se>
2022-10-01 13:36:28 +0200

    Change all emoji object files to emoji category classes

--- testApp/index.js
+++ testApp/index.js
@@ -3,20 +3,20 @@ import { emojilib } from '../lib/index.js'
 const text = emojilib.replaceEmoticonWithEmoji('hej :D <3 :d Hur mår du? :D :p ..')
 const emoji = emojilib.getEmojiByTag('coffee')
 
-let emojis = ''
-for (const emoji of emojilib.getAllEmojis()) {
-  emojis += ' ' + emoji
-}
+// let emojis = ''
+// for (const emoji of emojilib.getAllEmojis()) {
+//   emojis += ' ' + emoji
+// }
 
 // let emojisFromInputText = ''
 // for (const emoji of emojilib.getEmojisThatMatchesText('sad')) {
 //   emojisFromInputText += ' ' + emoji
 // }
 
-// let emojisFromGivenCategories = ''
-// for (const emoji of emojilib.getEmojisOnlyFromCategory('activity')) {
-//   emojisFromGivenCategories += ' ' + emoji
-// }
+let emojisFromGivenCategories = ''
+for (const emoji of emojilib.getEmojisOnlyFromCategory('activity')) {
+  emojisFromGivenCategories += ' ' + emoji
+}
 
 // let tags = ''
 // for (const tag of emojilib.getAllTags()) {
@@ -30,7 +30,7 @@ const pElementForText = document.createElement('p')
 const pElementForTag = document.createElement('p')
 // const pElementForInputText = document.createElement('p')
 
-pElementForEmojis.textContent = emojis
+pElementForEmojis.textContent = emojisFromGivenCategories
 pElementForText.textContent = text
 pElementForTag.textContent = emoji
 // pElementForInputText.textContent = emojisFromInputText
